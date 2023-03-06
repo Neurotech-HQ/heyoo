540ad7ac7941b26ee9b116ebb19f2afe98cd583c
Filippo Romani <mail@filipporomani.it>
2023-03-06 08:43:00 +0100

    fix: remove templatev2 (useless)

--- heyoo/__init__.py
+++ heyoo/__init__.py
@@ -105,14 +105,7 @@ class WhatsApp(object):
         logging.error(f"Response: {r.json()}")
         return r.json()
 
-    def send_template(
-        self,
-        template: str,
-        recipient_id: str,
-        recipient_type="individual",
-        lang: str = "en_US",
-        components: List = None,
-    ):
+    def send_template(self, template, recipient_id, components, lang: str = "en_US"):
         """
         Sends a template message to a WhatsApp user, Template messages can either be;
             1. Text template
@@ -131,31 +124,6 @@ class WhatsApp(object):
             >>> whatsapp = WhatsApp(token, phone_number_id)
             >>> whatsapp.send_template("hello_world", "5511999999999", lang="en_US"))
         """
-        if components is None:  # TO NOT USE LIST AS DEFAULT, BECAUSE IT IS MUTABLE
-            components = []
-        data = {
-            "messaging_product": "whatsapp",
-            "recipient_type": recipient_type,
-            "to": recipient_id,
-            "type": "template",
-            "template": {
-                "name": template,
-                "language": {"code": lang},
-                "components": components,
-            },
-        }
-        logging.info(f"Sending template to {recipient_id}")
-        r = requests.post(self.url, headers=self.headers, json=data)
-
-        if r.status_code == 200:
-            logging.info(f"Template sent to {recipient_id}")
-            return r.json()
-        logging.info(f"Template not sent to {recipient_id}")
-        logging.info(f"Status code: {r.status_code}")
-        logging.error(f"Response: {r.json()}")
-        return r.json()
-
-    def send_templatev2(self, template, recipient_id, components, lang="en_US"):
         data = {
             "messaging_product": "whatsapp",
             "to": recipient_id,
